7d64eebd9a91f7ce2cb0a943e209dbab163e246f
Heber Sandoval <he3ers@gmail.com>
2020-01-18 07:03:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function greet(timeString) {
   
   if (hour < 12) {
     return 'Good Morning';
-  } else if (hour > 12 & timeString < 17) {
+  } else if (hour > 12 & hour < 17) {
     return 'Good Afternoon';
   } else {
     return 'Good Evening';
